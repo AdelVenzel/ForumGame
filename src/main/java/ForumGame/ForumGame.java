1c851cb5956ddd9e34d31ab1adcee21af96dc61d
Angelina <angelina.venzel999@gmail.com>
2024-07-18 18:24:21 +0200

    init commit

---
+++ src/main/java/ForumGame/ForumGame.java
@@ -0,0 +1,12 @@
+package ForumGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumGame {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumGame.class, args);
+    }
+}
